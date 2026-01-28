65c6c26acc2a01fcc0323c2fc5ebb74ee6d9bee3
ivancotacte <cotactearmenion@gmail.com>
2026-01-28 22:28:31 +0800

    [mod] added layouts/pages for admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,12 @@
-function App() {}
+import Routes from "@/pages/Routes.jsx";
+import { ThemeProvider } from '@/provider/theme-provider'
+
+function App() {
+    return (
+        <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        <Routes/>
+        </ThemeProvider>
+    )
+}
 
 export default App
---
+++ client/src/pages/Routes.jsx
@@ -0,0 +1,18 @@
+import { RouterProvider, createBrowserRouter } from "react-router"
+import NotFound from "@/pages/errors/not-found/page"
+import Dashboard_Admin from "@/pages/admin/dashboard/page"
+
+const Routes = () => {
+    const routesForPublic = [
+        { path: "*", element: <NotFound /> },
+        { path: "/admin/dashboard", element: <Dashboard_Admin /> }
+    ];
+
+    const router = createBrowserRouter([
+        ...routesForPublic,
+    ]);
+
+    return <RouterProvider router={router} />;
+};
+
+export default Routes;
